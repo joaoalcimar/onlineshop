72681fd768abc6642ab7fbe0c206cafaf4d80807
Joao Alcimar <joao.alcimar.junior@gmail.com>
2021-06-21 15:45:35 -0300

    Navigation

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:onlineshopflutter/tabs/home_tab.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  final _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        HomeTab()
+      ],
+    );
+  }
+}
