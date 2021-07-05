a3ae1336e567d143fc08f73a5a72f917e47d0411
Joao Alcimar <joao.alcimar.junior@gmail.com>
2021-07-05 15:10:44 -0300

    Category Tab

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:onlineshopflutter/tabs/category_tab.dart';
 import 'package:onlineshopflutter/tabs/home_tab.dart';
 import 'package:onlineshopflutter/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
-
   final _pageController = PageController();
 
   @override
@@ -12,12 +12,21 @@ class HomeScreen extends StatelessWidget {
       controller: _pageController,
       physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
-        Scaffold(body: HomeTab(), drawer: CustomDrawer(_pageController),
+        Scaffold(
+          body: HomeTab(),
+          drawer: CustomDrawer(_pageController),
         ),
-        Container(color: Colors.greenAccent,),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Categories"),
+            centerTitle: true,
+            backgroundColor: Color.fromARGB(255, 211, 118, 130),
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: CategoryTab(),
+        ),
         Container(color: Colors.yellow),
         Container(color: Colors.redAccent),
-
       ],
     );
   }
