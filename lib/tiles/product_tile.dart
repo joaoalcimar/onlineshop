9b8a28473d18577812fdb7a68eff25779889610f
Joao Alcimar <joao.alcimar.junior@gmail.com>
2021-07-08 16:01:20 -0300

    Product Cards in ListView

--- lib/tiles/product_tile.dart
+++ lib/tiles/product_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:onlineshopflutter/datas/product_data.dart';
+import 'package:onlineshopflutter/screens/products_screen.dart';
 
 class ProductTile extends StatelessWidget {
   final String type;
@@ -11,6 +12,9 @@ class ProductTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: (){
+        // Navigator.of(context).push(MaterialPageRoute(builder: (context) => ProductsScreen(data)));
+      },
       child: Card(
         child: type == "grid"
             ? Column(
@@ -44,7 +48,30 @@ class ProductTile extends StatelessWidget {
                   )
                 ],
               )
-            : Row(),
+            : Row(children: <Widget>[
+              // fix variabilidade tamanho do dispositivo
+              Flexible(flex: 1, child: Image.network(
+            data.images![0],fit: BoxFit.cover, height: 250.0,)),
+              Flexible(flex: 1, child: Container(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(data.title.toString(),
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0)),
+                    Text(
+                      "R\$" + data.price.toString(),
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: Color.fromARGB(255, 211, 118, 130),
+                          fontSize: 17.0),
+                    )
+                  ],
+                ),
+              )),
+        ],
+
+        ),
       ),
     );
   }
